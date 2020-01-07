666b82c3dbf09f98a9293d7c853e36eca628507a
Daniel Shapiro <d.shapiro@berkeley.edu>
2020-01-06 17:18:56 -0800

    some stuff

--- objTo3dsmax.py
+++ objTo3dsmax.py
@@ -13,40 +13,58 @@ def parse_obj(inp_file):
             if line.strip().startswith("v"):
                 split = line.strip().split()
                 if (len(split) >= 4):
-                    obj.append((split[1], split[2], split[3]))
+                    obj.append((float(split[1]), float(split[2])))
 
     if len(obj) > 0:
         objs.append(obj)
     return objs
 
-def setup_solver(base_file):
+
+def setup_solvers(base_file):
     shapes = parse_obj(base_file)
     base_shape = shapes[0]
     targets = shapes[1:]
-    return Solver(base_shape, targets)
+    return Solver(base_shape, targets), Solver2(base_shape, targets)
 
 
 args = sys.argv
 base_file = "targets.obj"
 desireds_file = "desireds.obj"
-out_file = "out.csv"
+out_file_1 = "out1.ms"
+out_file_2 = "out2.ms"
 if len(args) > 1:
     base_file = args[1]
 if len(args) > 2:
     desireds_file = args[2]
 if len(args) > 3:
-    out_file = args[3]
+    out_file_1 = args[3]
+if len(args) > 4:
+    out_file_2 = args[4]
 
-solver = setup_solver(base_file)
-unused_inds = solver.unused_shape_inds
+solvers = setup_solvers(base_file)
+solver1 = solvers[0]
+solver2 = solvers[1]
+# unused_inds = solver.unused_shape_inds
 desired_shapes = parse_obj(desireds_file)
 
-out = open(out_file, "w")
+out = open(out_file_1, "w")
 
-for desired_shape in desired_shapes:
-    weights = solver.solve(desired_shape)
-    line = ",".join([str(weight) for weight in weights])
-    out.write(line)
-    out.write("\n")
+if len(desired_shapes) > 0:
+    weights = solver1.solve(desired_shapes[0])
+    for i in range(0, len(weights)):
+        out.write("WM3_MC_SetValue$BASE.morpher " + str(i+1) + " " + str(weights[i] * 100) + "\n")
+
+# for desired_shape in desired_shapes:
+#     weights = solver.solve(desired_shape)
+#     line = ",".join([str(weight) for weight in weights])
+#     out.write(line)
+#     out.write("\n")
 
 out.close()
+
+out = open(out_file_2, "w")
+
+if len(desired_shapes) > 0:
+    weights = solver2.solve(desired_shapes[0])
+    for i in range(0, len(weights)):
+        out.write("WM3_MC_SetValue$BASE.morpher " + str(i+1) + " " + str(weights[i] * 100) + "\n")
