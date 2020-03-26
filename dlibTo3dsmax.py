e1e4e06b21a5f93951948c1fefbd04a2297f7b22
Daniel Shapiro <d.shapiro@berkeley.edu>
2020-03-25 19:29:48 -0700

    changed input format

--- dlibTo3dsmax.py
+++ dlibTo3dsmax.py
@@ -1,12 +1,18 @@
 import sys
+import os
 from invMorph import *
 
 facepoint_prefix = "face point :"
 fp_prefix_len = len(facepoint_prefix)
 
 
-def parse_shapes(inp_file):
-    shapes = []
+def get_shape_files(shapes_dir):
+    (_, _, filenames) = os.walk(shapes_dir).next()
+    txtfiles = [shapes_dir + "/" + f for f in filenames if f.endswith(".txt")]
+    return txtfiles
+
+
+def parse_shape(inp_file):
     shape = []
     with open(inp_file, "r") as inp:
         for line in inp:
@@ -17,35 +23,44 @@ def parse_shapes(inp_file):
                     shape.append((float(split[3]), float(split[1])))
                 else:
                     shape.append((float(split[1]), float(split[3])))
-            else:
-                if len(shape) > 0:
-                    shapes.append(shape)
-                    shape = []
+    return shape
 
-    if len(shape) > 0:
-        shapes.append(shape)
+def parse_shapes(inp_files):
+    shapes = []
+    for inp_file in inp_files:
+        shapes.append(parse_shape(inp_file))
     return shapes
 
 args = sys.argv
-base_file = "base_and_targets"
-desireds_file = "desireds"
+# base_file = "base_and_targets"
+# desireds_file = "desireds"
+input_dir = "input"
 out_file = "out.ms"
 base_shape_name = "BASE"
 if len(args) > 1:
-    base_file = args[1]
+    input_dir = args[1]
 if len(args) > 2:
-    desireds_file = args[2]
+    out_file = args[2]
 if len(args) > 3:
-    out_file = args[3]
-if len(args) > 4:
-    base_shape_name = args[4]
+    base_shape_name = args[3]
 
-btshapes = parse_shapes(base_file)
-base_shape = btshapes[0]
-targets = btshapes[1:]
+base_dir = input_dir + "/base"
+morphs_dir = input_dir + "/morphset"
+desireds_dir = input_dir + "/desired"
+
+base_files = get_shape_files(base_dir)
+morph_files = sorted(get_shape_files(morphs_dir))
+desired_files = sorted(get_shape_files(desireds_dir))
+
+assert len(base_files) == 1
+assert len(morph_files) > 0
+assert len(desired_files) > 0
+
+base_shape = parse_shape(base_files[0])
+targets = parse_shapes(morph_files)
 solver = Solver2(base_shape, targets)
 
-desired_shapes = parse_shapes(desireds_file)
+desired_shapes = parse_shapes(desired_files)
 
 out = open(out_file, "w")
 
